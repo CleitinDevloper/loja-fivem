d1b22418f2c984c56873b3e068eae6764d308004
CleitinDevloper <mrsaskejogos@gmail.com>
2025-12-05 13:41:16 -0300

    Upando Site

---
+++ index.js
@@ -0,0 +1,175 @@
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const axios = require("axios");
+const crypto = require("crypto");
+const { createClient } = require('@supabase/supabase-js');
+const { stat } = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+async function sendMessageToFivem(action, infos, userId, token) {
+  try{
+    const response = await axios.post(
+      "http://181.214.221.132:8080/sendMessage",
+      {
+        action, infos, userId, token
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
+
+    return response.data.message;
+  }catch(e){
+    return false
+  };
+};
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const products = {
+    [1]: { id: 1, name: 'Gemas', category: 'gemas', price: 50, img: 'https://via.placeholder.com/300x200/000/00aaff?text=VIP+Pack' },
+};
+
+var pedidos = {}
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+function generateToken(size){
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+
+  var token = "";
+
+  for (var i = 0; i < size; i++){
+    const newChar = Math.floor(Math.random() * chars.length);
+
+    token += chars[newChar];
+  };
+
+  return token;
+};
+
+async function iniciar(){
+  const { data, error } = await supabase
+    .from('pedidos')
+    .select('*')
+
+  data.map(pedido => {
+    pedidos[pedido.id] = { token: pedido.token, status: pedido.status, price: pedido.price, cart: JSON.parse(pedido.cart), player_id: pedido.player_id }
+  });
+};
+
+iniciar();
+
+app.post('/api/webhook', async (req, res) => {
+  try{
+    const paymentId = req.body.data?.id;
+    if (!paymentId) return res.sendStatus(400);
+
+    const response = await axios.get(
+      `https://api.mercadopago.com/v1/payments/${paymentId}`,
+      {
+        headers: { Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}` }
+      }
+    );
+
+    const data = response.data;
+    var newStatus = "";
+
+    switch (data.status){
+      case "approved":
+        newStatus = "Pagamento Aprovado.";
+        break;
+      case "pending":
+        newStatus = "Aguardando Pagamento.";
+        break;
+      default:
+        newStatus = "Pagamento Cancelado.";
+        break;
+    };
+
+    if (newStatus != pedidos[data.external_reference].status){
+      const { error } = await supabase
+        .from('pedidos')
+        .update({
+          status: newStatus
+        })
+        .eq('id', data.external_reference);
+
+      if (newStatus == "Pagamento Aprovado."){
+        await sendMessageToFivem('loja_buy', { cart: JSON.stringify(pedidos[data.external_reference].cart), id: pedidos[data.external_reference].player_id }, `${data.external_reference}`, "onda_2020")
+      };
+    };
+  } catch(e){
+    console.log(e)
+  }
+});
+
+app.post('/api/checkonline', async (req, res) => {
+  const result = await sendMessageToFivem('server_on', {}, "1092124363886690364", "onda_2020");
+
+  return res.json({ status: result })
+})
+
+app.post('/api/checkout', async (req, res) => {
+  const { id, email, cart } = req.body;
+
+  try{
+    var newPrice = 0
+
+    for (const item of cart){
+      if (products[item.id].price) {
+        newPrice += products[item.id].price;
+      };
+    };
+
+    const paymentID = generateToken(35);
+    const idempotencyKey = crypto.randomUUID();
+
+    const { data: supaData, error } = await supabase
+      .from('pedidos')
+      .insert({ token: paymentID, status: "Aguardando Pagamento", price: newPrice, cart: JSON.stringify(cart), player_id: id })
+      .select();
+
+    const orderID = supaData[0].id;
+
+    if (orderID){
+      const response = await axios.post(
+        "https://api.mercadopago.com/v1/payments",
+        {
+          transaction_amount: parseFloat(newPrice),
+          description: "Pagamento via PIX",
+          payment_method_id: "pix",
+          payer: { email: email },
+          external_reference: orderID,
+          notification_url: "https://ondabrava.discloud.app/api/webhook",
+          installments: 1
+        },
+        {
+          headers: {
+            "Authorization": `Bearer ${process.env.MP_ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+            "x-idempotency-key": idempotencyKey
+          }
+        }
+      );
+
+      const data = response.data;
+
+      const qrCodeBase64 = data.point_of_interaction?.transaction_data?.qr_code_base64;
+      const qrCodeText = data.point_of_interaction?.transaction_data?.qr_code;
+
+      return res.json({ status: true, order_id: orderID, order_status: "Aguardando Pagamento", base64: qrCodeBase64, copiaecola: qrCodeText })
+    };
+  } catch(e){
+    console.log(e)
+    return res.json({ status: false, message: "Infelismente obtivemos um erro ao gerar seu pagamento, tente novamente mais tarde." })
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
---
+++ public/main.js
@@ -0,0 +1,255 @@
+const products = [
+    { id: 1, name: 'Gemas', desc: "", category: 'gemas', price: 50.0, img: 'https://via.placeholder.com/300x200/000/00aaff?text=VIP+Pack', obs: { enabled: true, max: 2, title: "Selecione até 2 carros", options: ["Carro A", "Carro B", "Carro C", "Carro D"]} },
+];
+
+let cart = [];
+let currentCategory = 'all';
+let searchQuery = '';
+
+function renderProducts() {
+    const container = document.getElementById('productsContainer');
+    container.innerHTML = '';
+    const filtered = products.filter(p =>
+        (currentCategory === 'all' || p.category === currentCategory) &&
+        p.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    filtered.forEach(product => {
+        const productEl = document.createElement('div');
+        productEl.className = 'product';
+        productEl.innerHTML = `
+                    <img src="${product.img}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>Descrição: ${product.desc}</p>
+                    <p>Preço: R$ ${product.price}</p>
+                    <button class="btn" onclick="addToCart(${product.id})">Adicionar ao Carrinho</button>
+                `;
+        container.appendChild(productEl);
+    });
+}
+
+function filterCategory(category) {
+    currentCategory = category;
+    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+    event.target.classList.add('active');
+    renderProducts();
+}
+
+document.getElementById('searchInput').addEventListener('input', function () {
+    searchQuery = this.value;
+    renderProducts();
+});
+
+let selectedProduct = null;
+
+function addToCart(id) {
+    const product = products.find(p => p.id === id);
+
+    if (product.obs && product.obs.enabled) {
+        selectedProduct = product;
+        openObsModal(product);
+        return;
+    }
+
+    pushToCart(product);
+}
+
+function pushToCart(product, obs = []) {
+    const existing = cart.find(item => item.id === product.id && JSON.stringify(item.obs_selected) === JSON.stringify(obs));
+
+    if (existing) {
+        existing.quantity++;
+    } else {
+        cart.push({
+            ...product,
+            quantity: 1,
+            obs_selected: obs
+        });
+    }
+
+    updateCart();
+}
+
+function updateCart() {
+    document.getElementById('cartCount').textContent = cart.reduce((sum, item) => sum + item.quantity, 0);
+    const cartItems = document.getElementById('cartItems');
+    cartItems.innerHTML = '';
+    let total = 0;
+    cart.forEach(item => {
+        total += item.price * item.quantity;
+        const itemEl = document.createElement('div');
+        itemEl.className = 'cart-item';
+        itemEl.innerHTML = `
+                    <span>${item.name} (x${item.quantity}) - R$ ${item.price * item.quantity},00</span>
+                    <button onclick="removeFromCart(${item.id})">Remover</button>
+                `;
+        cartItems.appendChild(itemEl);
+    });
+    document.getElementById('cartTotal').textContent = `Total: R$ ${total},00`;
+}
+
+function removeFromCart(id) {
+    cart = cart.filter(item => item.id !== id);
+    updateCart();
+}
+
+function openCart() {
+    document.getElementById('cartModal').style.display = 'block';
+}
+
+function closeCart() {
+    document.getElementById('cartModal').style.display = 'none';
+}
+
+async function checkout() {
+    document.getElementById("loadModal").style.display = "block";
+    
+    if (cart.length === 0) {
+        alert('Carrinho vazio!');
+        document.getElementById("loadModal").style.display = "none";
+        return;
+    }
+
+    closeCart();
+
+    const res = await fetch('/api/checkonline', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    });
+
+    const data = res.json();
+
+    if (data) {
+        document.getElementById('purchaseModal').style.display = 'block';
+        document.getElementById("loadModal").style.display = "none";
+    } else {
+        alert('Cidade offline no momento aguarde um pouco e tente novamente')
+        document.getElementById("loadModal").style.display = "none";
+        return;
+    };
+}
+
+async function confirmPurchase() {
+    const gameId = document.getElementById('gameId').value;
+    const email = document.getElementById('email').value;
+
+    if (gameId.trim() === '') {
+        alert('Por favor, insira seu ID dentro do jogo.');
+        return;
+    }
+
+    if (email.trim() === ''  || !(email.includes("@")) || !(email.includes("."))) {
+        alert('Por favor, insira um email valido.');
+        return;
+    }
+
+    const res = await fetch('/api/checkout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: gameId, email: email, cart: cart })
+    });
+
+    const data = await res.json();
+
+    if (data.status) {
+        closeModal()
+
+        document.getElementById("paymentStatus").innerText = `Status: ${data.order_status}`;
+        document.getElementById("paymentCode").innerText = `Codigo do seu Pedido: ${data.order_id}`;
+        document.getElementById("paymentModal").style.display = "block";
+        document.getElementById("qrcode").src = `data:image/png;base64,${data.base64}`;
+        document.getElementById("copiaecola").innerText = data.copiaecola;
+
+        setCookie("order_id", data.order_id, 7)
+    } else{
+        alert("Houve um erro, tente novamente mais tarde.")
+    }
+
+    cart = [];
+
+    updateCart();
+}
+
+function closeModal() {
+    document.getElementById('purchaseModal').style.display = 'none';
+}
+
+function openObsModal(product) {
+    document.getElementById('obsTitle').innerText = product.obs.title;
+    const box = document.getElementById('obsOptions');
+    box.innerHTML = '';
+
+    product.obs.options.forEach(option => {
+        const item = document.createElement('div');
+        item.innerHTML = `
+            <label>
+                <input type="checkbox" name="obsOption" value="${option}">
+                ${option}
+            </label>
+        `;
+        box.appendChild(item);
+    });
+
+    document.getElementById('obsModal').style.display = "block";
+}
+
+function closeObs() {
+    document.getElementById('obsModal').style.display = "none";
+}
+
+function confirmObs() {
+    const checkboxes = document.querySelectorAll('input[name="obsOption"]:checked');
+    const values = Array.from(checkboxes).map(c => c.value);
+
+    if (values.length > selectedProduct.obs.max) {
+        alert(`Você só pode selecionar até ${selectedProduct.obs.max} opções.`);
+        return;
+    }
+
+    pushToCart(selectedProduct, values);
+
+    closeObs();
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('cartModal')) {
+        closeCart();
+    }
+    if (event.target == document.getElementById('purchaseModal')) {
+        closeModal();
+    }
+}
+
+document.getElementById("copy").addEventListener("click", function(e){
+    navigator.clipboard.writeText(document.getElementById("copiaecola").value);
+})
+
+const tabs = document.getElementsByClassName('tab') 
+
+for (let tab of tabs) {
+    tab.addEventListener('click', function() {
+        filterCategory(this.getAttribute('data-tab'));
+    });
+}
+
+function setCookie(name, value, days = 7) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = `${name}=${encodeURIComponent(value || "")}${expires}; path=/`;
+}
+
+document.getElementById('openCart').addEventListener('click', openCart);
+document.getElementById('closeCart').addEventListener('click', closeCart);
+document.getElementById('checkoutBtn').addEventListener('click', checkout);
+document.getElementById('confirmPurchaseBtn').addEventListener('click', confirmPurchase);
+document.getElementById('closeModal').addEventListener('click', closeModal);
+
+renderProducts();
